90dbc6c8246117cf92d358ab36cf7ab80e33987b
Made <49566185+made-o@users.noreply.github.com>
2022-07-15 15:54:05 -0300

    Update execute.v

--- src/execute.v
+++ src/execute.v
@@ -7,12 +7,12 @@ module execute #(
 )
    // Inputs & Outputs:
 (  input wire i_clk, i_reset, i_enable,
-   // Señales de control de la EX:
+   // SeÃ±ales de control de la EX:
    input [1:0] i_aluOP, // 2-bits
    input i_aluSrc,
    input i_regDst,
    
-   // Señales de control de la unidad de Cortorcircuito:
+   // SeÃ±ales de control de la unidad de Cortorcircuito:
    input i_forwardA,
    input i_forwardB,
    
@@ -25,19 +25,19 @@ module execute #(
    input [N_BITS_REG-1:0] i_rt_id, // 6-bits
    input [N_BITS_REG-1:0] i_rd_id, // 6-bits
    
-   // Señal de entrada al módulo de control de la ALU
+   // SeÃ±al de entrada al mÃ³dulo de control de la ALU
    input [N_BITS_REG:0]   i_opcode, // 6bits
    
-   // Señales extras que entran a la unidad de cortocircuito:
+   // SeÃ±ales extras que entran a la unidad de cortocircuito:
    input [N_BITS_REG-1:0] i_rd_EX_MEM, //6-bits
    input [N_BITS_REG-1:0] i_rd_MEM_WB, //6-bits
    input [N_BITS_REG-1:0] i_rs_id,     //6-bits
    
-   // Señales que vienen de las etapas WB y MEM (para usar en MUX)
+   // SeÃ±ales que vienen de las etapas WB y MEM (para usar en MUX)
    input [N_BITS-1:0] i_wbData,
    input [N_BITS-1:0] i_memData,
    
-   // Señales que vienen de la etapa ID o de etapas siguientes:
+   // SeÃ±ales que vienen de la etapa ID o de etapas siguientes:
    input i_memToReg,
    input i_regWrite_EX_MEM,
    input i_regWrite_MEM_WB,
@@ -47,10 +47,10 @@ module execute #(
    
    
    output reg  o_ceroSignal,
-   output reg  [N_BITS-1:0] aluResult,
-   output wire [N_BITS-1:0] datoLeido2,
-   output wire [N_BITS_REG-1:0] rt_or_rd_data,
-   
+   output reg  [N_BITS-1:0] o_aluResult,
+   output wire [N_BITS-1:0] o_datoLeido2,
+   output wire [N_BITS_REG-1:0] o_rt_data, //6-bits
+   output wire [N_BITS_REG-1:0] o_rd_data, //6-bits
    output reg o_memToReg,
    output reg o_regWrite_EX_MEM,
    output reg o_regWrite_MEM_WB,
@@ -61,20 +61,23 @@ module execute #(
 );
 
    // Internal Variables:
-   // Señales referidas al bloque ALU:
+   // SeÃ±ales referidas al bloque ALU:
    reg [N_BITS-1:0] dato1ALU; // DatoA que ingresa a la ALU
    reg [N_BITS-1:0] dato2ALU; // DatoB que ingresa a la ALU
    reg [N_BITS-1:0] dato2_preALU; // Dato que sale del MUX-forwardB
    reg [N_BITS_REG-1:0] aluOpcode; // 6-bits que salen del bloque de control e ingresan a la ALU
-   reg [N_BITS_REG-1:0] rt_or_rd_data_toMem; // RT o RD que salen del mux
+   //reg [N_BITS_REG-1:0] rt_or_rd_data_toMem; // RT o RD que salen del mux
    
-   // Señales referidas a la unidad de encaminamiento/cortocircuito:
+   // SeÃ±ales referidas a la unidad de encaminamiento/cortocircuito:
    reg memToReg;
    reg regWrite_EX_MEM;
    reg regWrite_MEM_WB;
    reg branch;
    reg memWrite;
    reg memRead;
+   
+   reg rt_data;
+   reg rd_data;
    
    ////////////////////////////////////////////////////
    // Start-code:
@@ -106,20 +109,22 @@ module execute #(
    end//end_always
    
    // Multiplexor for RegDst:
+   /*
    always @(i_rt_id or i_rd_id or i_regDst) begin
       case(i_regDst)
          2'b00: rt_or_rd_data_toMem <= i_rt_id;
          2'b01: rt_or_rd_data_toMem <= i_rd_id;
       endcase
    end//end_always
+   */
    
-   
-   // Asigno a la salida del dato2 y [rd o rd] para la etapa siguiente:
+   // Asigno a la salida:
    assign datoLeido2 = dato2_preALU;
-   assign rt_or_rd_data = rt_or_rd_data_toMem;
+   assign o_rt_data = i_rt_id;
+   assign o_rd_data = i_rd_id;
    
-   
-   always@(posedge i_clk) begin // FIJARSE - si faltan variables para setear en cero
+   //--------------------------------------------
+   always@(posedge i_clk) begin: lectura// FIJARSE - si faltan variables para setear en cero
       if(i_reset) begin
          memToReg <= 1'b0;
          regWrite_EX_MEM <= 1'b0;
@@ -127,6 +132,7 @@ module execute #(
          branch   <= 1'b0;
          memWrite <= 1'b0;
          memRead  <= 1'b0;
+         aluOpcode<= {N_BITS_REG{1'b0}};
       end//end_if
       else begin
          memToReg <= i_memToReg;
@@ -135,13 +141,16 @@ module execute #(
          branch   <= i_branch;
          memWrite <= i_memWrite;
          memRead  <= i_memRead;
+         aluOpcode<= i_opcode;
       end//end_else
    end//end_always
    
-   always@(negedge i_clk) begin
+   //--------------------------------------------
+   always@(negedge i_clk) begin: escritura
       if(i_reset) begin
          o_memToReg <= 1'b0;
          o_regWrite_EX_MEM <= 1'b0;
+         o_regWrite_MEM_WB <= 1'b0;
          o_branch   <= 1'b0;
          o_memWrite <= 1'b0;
          o_memRead  <= 1'b0;
@@ -158,7 +167,7 @@ module execute #(
    
    
    ////////////////////////////////////////////////////
-   // Instanciacion de módulo de Control de ALU:
+   // Instanciacion de mÃ³dulo de Control de ALU:
    aluControl
    u_aluBlock (
       .i_aluOp(i_aluOP),  // 2bits
@@ -168,7 +177,7 @@ module execute #(
       .o_opcodeAlu(aluOpcode) // 32bits
    );
 
-   // Instanciacion de módulo de ALU:
+   // Instanciacion de mÃ³dulo de ALU:
    alu
    u_alu_1(
       .i_datoA(dato1ALU),
@@ -179,8 +188,8 @@ module execute #(
       .o_cero(o_ceroSignal)
    );
    
-   // Instanciacion de módulo de Cortocircuito:
-   cortocircuito
+   // Instanciacion de mÃ³dulo de Cortocircuito:
+   fowarding_unit
    u_corto (
       .i_rt_id(i_rt_id),
       .i_rs_id(i_rs_id),
@@ -192,7 +201,5 @@ module execute #(
       .o_forwardA(i_forwardA),
       .o_forwardB(i_forwardB)
    );
-   
-   
    
 endmodule
