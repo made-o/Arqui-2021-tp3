882df0a1e7f65b96884091ee70bbb09a204885ef
GuillermoJohnston <64984209+GuillermoJohnston@users.noreply.github.com>
2023-09-12 16:20:04 -0300

    Update pc_jump.v

--- src/pc_jump.v
+++ src/pc_jump.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 28.07.2022 16:50:34
-// Design Name: 
-// Module Name: pc_jump
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module pc_jump#
 (
@@ -35,6 +15,6 @@ module pc_jump#
  );
  
  assign o_sign_extension = {{(N_BITS_W){i_sign_extension[15]}},i_sign_extension};
- assign o_jump_direction = pc + (o_sign_extension<<2);
+ assign o_jump_direction = pc + ({{(N_BITS_W){i_sign_extension[15]}},i_sign_extension}<<2);
  
 endmodule
